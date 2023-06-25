f256e8b298e93c6f0e337a72b42fcbeb906227e8
rajathbharadwaj <rajathrajath940@gmail.com>
2023-06-25 17:09:34 +0530

    fix envs

--- intelligence/withlc.py
+++ intelligence/withlc.py
@@ -2,8 +2,7 @@ import os
 from langchain import PromptTemplate, OpenAI, LLMChain
 import chainlit as cl
 
-os.environ["OPENAI_API_KEY"] = "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"
-os.environ["SERPAPI_API_KEY"] = "70035b12a2500be93964ae7a61a2bfe4a1d98b09f651963376b6c7ae84a9db9d"
+
 
 template = """Question: {question}
 
