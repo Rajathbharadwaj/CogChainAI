4602477cc3d19f20d6e79696edf91b643d394b89
rajathbharadwaj <rajathrajath940@gmail.com>
2023-06-25 17:09:06 +0530

    fix envs

--- intelligence/mrkl_new.py
+++ intelligence/mrkl_new.py
@@ -4,9 +4,9 @@ from langchain.chat_models import ChatOpenAI
 import os
 import chainlit as cl
 
-os.environ["OPENAI_API_KEY"] = "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"
-os.environ["SERPAPI_API_KEY"] = "70035b12a2500be93964ae7a61a2bfe4a1d98b09f651963376b6c7ae84a9db9d"
 
+OPENAI_API_KEY=os.environ["OPENAI_API_KEY"] 
+SERPAPI_API_KEY=os.environ["SERPAPI_API_KEY"]
 
 # The search tool has no async implementation, we fall back to sync
 @cl.langchain_factory(use_async=False)
